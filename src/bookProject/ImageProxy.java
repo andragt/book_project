b4061ade1014a2803a6c636c7f80dc3f0f139b28
andragt <andra.tigae97@e-uvt.ro>
2018-12-18 21:49:10 +0200

    fourth lab

---
+++ src/bookProject/ImageProxy.java
@@ -0,0 +1,43 @@
+package bookProject;
+
+public class ImageProxy implements Element {
+	
+	private String imageName;
+	private Image realImage = null;
+	
+
+	
+	public ImageProxy(String imageName) {
+		this.imageName = imageName;
+	}
+
+
+	public void print() {
+		if (realImage == null) { 
+			realImage = new Image(imageName);
+		}
+		realImage.print();
+	}
+
+
+	@Override
+	public void add(Element elm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void remove(Element elm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Element getChild(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/bookProject/Main.java
+++ src/bookProject/Main.java
@@ -2,35 +2,35 @@ package bookProject;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-			/*Book discoTitanic = new Book("Disco Titanic");
-			Author rpGheo = new Author("Radu Pavel Gheo");
-				
-			discoTitanic.addAuthor(rpGheo);
-			int indexChapterOne = discoTitanic.createChapter("Capitolul 1");
-			Chapter chp1 = discoTitanic.getChapter(indexChapterOne);
-			int indexSubChapterOneOne = chp1.createSubChapter("Subcapitolul 1.1");
-			SubChapter scOneOne = chp1.getSubChapter(indexSubChapterOneOne);
-				
-			scOneOne.createNewParagraph("Paragraph 1");
-			scOneOne.createNewParagraph("Paragraph 2");
-			scOneOne.createNewParagraph("Paragraph 3");
-			scOneOne.createNewImage("Image 1");
-			scOneOne.createNewParagraph("Paragraph 4");
-			scOneOne.createNewTable("Table 1");
-			scOneOne.createNewParagraph("Paragraph 5");
-			discoTitanic.print();*/
-			Book mathBook = new Book("Base Math");
-			mathBook.add(new Paragraph("Thanks to my parents"));
-			Section cap1 = new Section("Basic Math");
-			cap1.add(new Paragraph("Bla Bla 2"));
-			Section cap11 = new Section("Set parameters");
-			cap1.add(cap11);
-			Section cap111 = new Section("Finish");
-			cap11.add(cap111);
-			mathBook.add(cap1);
-			mathBook.print();
+		long startTime = System.currentTimeMillis();
+		ImageProxy img1 = new ImageProxy("Pamela Anderson");
+		ImageProxy img2 = new ImageProxy("Kim Kardashian");
+		ImageProxy img3 = new ImageProxy("Kirby Griffin");
+		Section playboyS1 = new Section("Front Cover");
+		playboyS1.add(img1);
+		Section playboyS2 = new Section("Summer Girls");
+		playboyS2.add(img2);
+		playboyS2.add(img3);
+		Book playboy = new Book("Playboy");
+		playboy.add(playboyS1);
+		playboy.add(playboyS2);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Creation of the content took " + (endTime - startTime) + " milliseconds");
 		
-		}
+		startTime = System.currentTimeMillis();
+		playboyS1.print();
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Printing of the section 1 took " + (endTime - startTime) + " milliseconds");
+			
+		startTime = System.currentTimeMillis();
+		playboyS1.print();
+		endTime = System.currentTimeMillis();
+		
+		System.out.println("Printing again the section 1 took " + (endTime - startTime) + " milliseconds");
+	}
+
+
+		
 }
